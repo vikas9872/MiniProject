42dfc4d37a1444a3ee5b542e049c2fd0205744ae
Vikas_Shanabhog <119601456+vikas9872@users.noreply.github.com>
2024-11-01 10:31:00 +0530

    Update dbConn.js

--- backEnd/Config/dbConn.js
+++ backEnd/Config/dbConn.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/FoodDeliveryDb")
+        await mongoose.connect("TYPE_YOUR_DATABASE_LINK_HERE")
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB', error);
