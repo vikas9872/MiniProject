020ae73b0c6f9aff8bf8509ce33377e235f005ab
Vikas_Shanabhog <119601456+vikas9872@users.noreply.github.com>
2025-06-30 13:08:27 +0530

    Update OrderController.js

--- backEnd/Controllers/OrderController.js
+++ backEnd/Controllers/OrderController.js
@@ -7,7 +7,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // placing user order for frontend
 const  placeOrder=async(req,res)=>{
-    const frontendurl="https://foodcourttt.onrender.com/"
+    const frontendurl="https://fooodcourttt.vercel.app"
     try{
         const newOrder=new orderModel({
             userId: req.body.userId,
