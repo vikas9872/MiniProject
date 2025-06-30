82e947b096f32993f05253ec09b648aeeb61a1c9
Vikas_Shanabhog <119601456+vikas9872@users.noreply.github.com>
2025-06-30 13:03:11 +0530

    Update Server.js

--- backEnd/Server.js
+++ backEnd/Server.js
@@ -34,7 +34,3 @@ app.get("/",(req,res)=>{
 app.listen(port,()=>{
     console.log(`Server started on http://localhost:${port}`)
 })
-
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`)
-})
